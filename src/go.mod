d240c508e9195cb4026c32a4ce448498ed15a668
iPilotte <pm07.privatemonster@gmail.com>
2019-04-12 23:57:28 +0700

    +Post(helloname), +connectDB

--- src/go.mod
+++ src/go.mod
@@ -5,7 +5,9 @@ go 1.12
 require (
 	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
 	github.com/gin-gonic/gin v1.3.0
+	github.com/go-sql-driver/mysql v1.4.0
 	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/mattn/go-isatty v0.0.7 // indirect
 	github.com/ugorji/go v1.1.4 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
--- src/main.go
+++ src/main.go
@@ -1,15 +1,85 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql" //ไม่ตั้งชื่อให้โดยใช้ _
+	"github.com/jmoiron/sqlx"
+)
+
+//config file
+const (
+	username = "root"
+	password = "hello1234"
+	host     = "localhost"
+	port     = "3306"
+	database = "helloworld"
+)
+
+// Hello is contain Name GLOBAL VALUE JAA IF PRIVATE IS name
+type Hello struct {
+	Name string `json:"name"`
+}
+
+// Register model
+type Register struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// User model
+type User struct {
+	UID      string `db:"uid"`
+	Username string `db:"username"`
+	Password string `db:"password"`
+}
 
 func main() {
+	urlSql := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", username, password, host, port, database)
+	_, err := sqlx.Connect("mysql", urlSql)
+	if err != nil {
+		log.Println(err)
+		log.Fatal("Can not Connet Database")
+	}
+	//route
 	route := gin.Default()
-	route.GET("/hello", helloworld)
+	route.GET("/hello", helloworldHandler)     //END POINT JAA
+	route.POST("/helloname", helloNameHandler) //END POINT JAA
+	route.POST("/register", registerHandler)   //END POINT JAA
 	route.Run(":3000")
 }
 
-func helloworld(context *gin.Context) {
-	context.JSON(200, gin.H{
+func helloworldHandler(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
 		"message": "hello world",
 	})
+}
+
+func helloNameHandler(context *gin.Context) {
+	var helloName Hello
+	err := context.ShouldBindJSON(&helloName)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "input incorrect",
+		})
+		return
+	}
+	helloName.Name = "hello " + helloName.Name
+	context.JSON(http.StatusOK, helloName)
+}
+
+func registerHandler(context *gin.Context) {
+	var helloName Hello
+	err := context.ShouldBindJSON(&helloName)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "input incorrect",
+		})
+		return
+	}
+	helloName.Name = "hello " + helloName.Name
+	context.JSON(http.StatusOK, helloName)
 }
